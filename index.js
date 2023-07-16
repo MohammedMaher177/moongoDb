e4e7bf25604633fbc08ff203b0eff2302b347a40
MohammedMaher177 <anahammo777@gmail.com>
2023-07-16 09:34:44 +0300

    cors

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import bootstrap from './src/app.router.js';
-
+import cors from 'cors'
 
 
 
 const app = express();
 const port = 5000
 
+app.use(cors())
 
 bootstrap(app, express)
 
